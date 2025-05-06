7a4e4e88348b032de7de2fb2c1f1e8509ac37fb0
ybabajan <96827681+ybabajan@users.noreply.github.com>
2025-05-06 10:20:38 +0300

    Add files via upload

---
+++ day2_streamlit.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from streamlit.logger import get_logger
+from langchain_huggingface import HuggingFaceEndpoint,ChatHuggingFace
+
+logger = get_logger(__name__)
+
+import os
+if os.getenv('USER', "None") == 'appuser': # streamlit
+    ht_token = st.secrets['HF_TOKEN']
+    os.environ["HUGGINGFACEHUB_API_TOKEN"] = hf_token
+else:
+    os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.environ["MY_HF_API_TOKEN"]
+
+st.title("my gen AI app")
+repo_id = "microsoft/Phi-3-mini-4k-instruct"
+temp = 1
+print(repo_id, temp)
+logger.info(f"{temp=}")
+
+with st.form("sample_app"):
+    txt = st.text_area("Enter text:", "what GPT stands for?")
+    sub = st.form_submit_button("submit")
+    if sub:
+        llm = HuggingFaceEndpoint(
+                repo_id=repo_id,  #3.8B model
+                task="text-generation",
+                temperature=temp
+                )
+        chat = ChatHuggingFace(llm=llm, verbose=True)
+        logger.info("invoking")
+        ans = chat.invoke(txt)
+        st.info(ans.content)
+        logger.info("Done")
+
